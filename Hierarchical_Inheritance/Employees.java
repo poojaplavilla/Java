99c3f2674f5fff6845243ac0377788f063d86fcf
poojaplavilla <50007653+poojaplavilla@users.noreply.github.com>
2019-04-29 23:32:42 +0530

    Update Employees.java

--- Hierarchical_Inheritance/Employees.java
+++ Hierarchical_Inheritance/Employees.java
@@ -7,15 +7,18 @@ public class Employees {
 	public Employees() {
 		
 	}
+	//These two variables are public and will be inheritated in both the child classes...
 	public Employees(int id, String name) {
 		this.id = id;
 		this.name = name;
 	}
 	
+	//this method will get override by the child classes...
 	void CalSalary() {
 		
 	}
 
+	//printinh purpose... 
 	void print() {
 		System.out.println("...EMPLOYEE DETAILS...\n");
 		System.out.println("Your id is " + id);
