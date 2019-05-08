a73de9837d596a07a5e3f9e36174d483e2bbbbfb
poojaplavilla <50007653+poojaplavilla@users.noreply.github.com>
2019-05-09 05:27:59 +0530

    Create TestEmployee.java

---
+++ containment_emp/TestEmployee.java
@@ -0,0 +1,13 @@
+package containment_emp;
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+		
+		Employee e = new Employee(1, "Pooja Plavilla", 90000f, new Certificate("BTech", "A+", "GTU"));
+		e.print();
+		
+
+	}
+
+}
