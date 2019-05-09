610fe2d5ed0430abad6d2499d35d87e06f9d481c
poojaplavilla <50007653+poojaplavilla@users.noreply.github.com>
2019-05-09 14:17:58 +0530

    Create DatabaseOpr.java

---
+++ jdbc/DatabaseOpr.java
@@ -0,0 +1,51 @@
+package jdbc_crud;
+
+import java.sql.*;
+
+public class DatabaseOpr {
+	Connection con;
+	Statement st;
+
+	public DatabaseOpr() throws Exception {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		System.out.println("Driver loaded...");
+
+		con = DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "");
+		System.out.println("Connection established...");
+	}
+
+	public void create() throws Exception {
+		String qu = "create table Employee(id integer primary key,EmpName varchar(100) not null,Salary integer not null);";
+		st = con.createStatement();
+		st.executeUpdate(qu);
+	}
+
+	public void insert() throws Exception {
+		String qu = "insert into Employee values (1,'abc',123456);";
+		st = con.createStatement();
+		st.executeUpdate(qu);
+	}
+
+	public void update() throws Exception {
+		String q = "update Employee set Empname='xyz' where id=1;";
+		st = con.createStatement();
+		st.executeUpdate(q);
+	}
+
+	public void delete() throws Exception {
+		String query = "delete from Employee where id = 1";
+		st = con.createStatement();
+		st.executeUpdate(query);
+	}
+
+	public void show() throws Exception {
+		st = con.createStatement();
+		ResultSet rs = st.executeQuery("select * from Employee;");
+
+		while (rs.next()) {
+			System.out.println(rs.getInt(1) + "," + rs.getString(2) + "," + rs.getInt(3));
+			
+		}
+	}
+}
