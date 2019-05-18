88bf2b0167968c2f9b7a19553a6dade08032474f
poojaplavilla <50007653+poojaplavilla@users.noreply.github.com>
2019-05-18 11:39:51 +0530

    Update TestCount.java

--- vowelcount/TestCount.java
+++ vowelcount/TestCount.java
@@ -12,6 +12,7 @@ public class TestCount
 				float vowels = 0;
 				float consonants = 0;
 				float space = 0;
+				float line = 0;
 				float pervow,percon,perspa;
 
 				while ((x = fis.read()) != -1) 
@@ -27,22 +28,33 @@ public class TestCount
 					{
 						space++;
 					}
+					else if(ch == '\n')
+					{
+						line++;
+					}
 					else
 					{
 						consonants++;
-					}
-					
+					}					
 				}
+				
 				x= vowels + consonants;
+				
 				System.out.println("\nTotal number of words: " + x);
 				System.out.println("\nNo. of vowels: " + vowels);
 				System.out.println("No. of consonants: " + consonants);
 				System.out.println("No. of space: " + space);
+				System.out.println("No. of line: " +  line);
+
 				pervow =(vowels/x) * 100;
 				System.out.println("Percentage of Vowels:" + pervow);
+				
 				percon = (consonants/x) * 100;
 				System.out.println("Percentage of Consonants: " + percon);
+				
 				perspa =(space/x) * 100;
 				System.out.println("Percentage of Space:" + perspa);
 			}
 		}
+
+	
