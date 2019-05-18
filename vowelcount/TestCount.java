f61e7ce7a63044d6e6e0842cf0bf0dc525c0c225
poojaplavilla <50007653+poojaplavilla@users.noreply.github.com>
2019-05-18 11:22:20 +0530

    Update TestCount.java

--- vowelcount/TestCount.java
+++ vowelcount/TestCount.java
@@ -6,40 +6,43 @@ public class TestCount
 {
 
 	public static void main(String[] args) throws IOException
-	{
-		
+	{		
 				FileInputStream fis = new FileInputStream("C:\\Program Files\\testf.txt");
-				int x = 0;
-				int vowels = 0;
-				int consonants = 0;
-				//float per=0;
+				float x = 0;
+				float vowels = 0;
+				float consonants = 0;
+				float space = 0;
+				float pervow,percon,perspa;
 
 				while ((x = fis.read()) != -1) 
-				{
-					//x++;
-				//	System.out.println("Total number of words: " + x);
-					
+				{			
 					System.out.print((char)x);
-					
+
 					char ch = (char) x;
 					if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') 
 					{
 						vowels++;
+					}
+					else if(ch == ' ') 
+					{
+						space++;
 					}
 					else
 					{
 						consonants++;
 					}
 					
-					//per = (vowels/x) * 100 ;
-					
 				}
-				//x++;
-				//System.out.println("Total number of words: " + x);
+				x= vowels + consonants;
+				System.out.println("\nTotal number of words: " + x);
 				System.out.println("\nNo. of vowels: " + vowels);
 				System.out.println("No. of consonants: " + consonants);
-				//System.out.println("Percentage of Vowels:" + per);
+				System.out.println("No. of space: " + space);
+				pervow =(vowels/x) * 100;
+				System.out.println("Percentage of Vowels:" + pervow);
+				percon = (consonants/x) * 100;
+				System.out.println("Percentage of Consonants: " + percon);
+				perspa =(space/x) * 100;
+				System.out.println("Percentage of Space:" + perspa);
 			}
 		}
-
-	
