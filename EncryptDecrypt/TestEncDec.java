b5608417806dc674064b3cf7ba2895c9b67ac200
poojaplavilla <50007653+poojaplavilla@users.noreply.github.com>
2019-05-20 18:15:48 +0530

    Create TestEncDec.java

---
+++ EncryptDecrypt/TestEncDec.java
@@ -0,0 +1,18 @@
+package encrptdecrypt;
+
+import java.io.IOException;
+
+public class TestEncDec {
+
+	
+		public static void main(String[] args) throws IOException {
+			Encrypt e = new Encrypt();
+			Decrypt d = new Decrypt();
+			e.encrypt();
+			d.decrypt();
+			
+		
+
+	}
+
+}
