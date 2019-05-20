dcb59d98d589e8d3cf6f87396c4236073adb5e6f
poojaplavilla <50007653+poojaplavilla@users.noreply.github.com>
2019-05-20 21:47:33 +0530

    Create Shape.java

---
+++ AbstractClass/Shape.java
@@ -0,0 +1,13 @@
+package abstractclass;
+
+public abstract class Shape {
+	float area;
+
+	public void print() {
+
+		System.out.println("Area is : " + area);
+	}
+
+	public abstract void Cal();
+
+}
