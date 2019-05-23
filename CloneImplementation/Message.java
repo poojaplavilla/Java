6e01fdc1b8e9886e166b5bbde97bfed9e5834344
poojaplavilla <50007653+poojaplavilla@users.noreply.github.com>
2019-05-24 00:28:06 +0530

    Update Message.java

--- CloneImplementation/Message.java
+++ CloneImplementation/Message.java
@@ -1,9 +1,27 @@
 package clone;
 
 public class Message implements Cloneable {
-	String msg;
+	private String msg;
 	
-	public Object clone() throws CloneNotSupportedException {
+	public Message() {
+		super();
+	}
+
+	public Message(String msg) {
+		super();
+		this.msg = msg;
+
+	}
+	
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+		public Object clone() throws CloneNotSupportedException {
 			System.out.println("Cloning...");
 			return super.clone();
 	}
